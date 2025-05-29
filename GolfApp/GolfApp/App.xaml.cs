3858516ce4b374e52ddfa37fcf23aaeadaeb1fdc
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2025-05-29 15:22:50 +0200

    app sln

---
+++ GolfApp/GolfApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GolfApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ GolfApp/GolfApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GolfApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
